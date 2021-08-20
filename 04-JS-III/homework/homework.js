2a9dfd57375e95f24c36babeb19cfbbb30437b0c
edgardomorales2021 <edmor2007@gmail.com>
2021-08-19 23:47:51 -0300

    Update homework.js

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -2,8 +2,8 @@
 
 function devolverPrimerElemento(array) {
   // Devuelve el primer elemento de un array
-  // Tu código:
-  return [0];
+  // Tu codigo:
+  return (array[0]);
 }
 
 
@@ -32,7 +32,6 @@ function incrementarPorUno(array) {
   }
     return NuevoArray;     
 }
-
 
 
 
@@ -132,15 +131,25 @@ function multiplicarArgumentos() {
 function cuentoElementos(arreglo){
   //Realiza una función que retorne la cantidad de los elementos del arreglo cuyo valor es mayor a 18.
   //Escribe tu código aquí
-
+  var contador=0;
+  for (let i=0; i<arreglo.length;i++){
+    if arreglo[i]>18 contador++
+  }
+  return contador;
 }
 
 
+
 function diaDeLaSemana(numeroDeDia) {
+  // 1=Domingo, 2=Lunes, 3=Martes...7=Sabado.
   //Suponga que los días de la semana se codifican como 1 = Domingo, 2 = Lunes y así sucesivamente. 
   //Realiza una función que dado el número del día de la semana, retorne: Es fin de semana
   //si el día corresponde a Sábado o Domingo y “Es dia Laboral” en caso contrario. 
   //Escribe tu código aquí   
+  if (numeroDeDia===1||numeroDeDia===7){
+    return "Es fin de semana";
+  }
+    else return "Es dia Laboral";
   
 } 
 
@@ -149,6 +158,10 @@ function empiezaConNueve(n) {
   //Desarrolle una función que recibe como parámetro un número entero n. Debe retornar true si el entero 
   //inicia con 9 y false en otro caso.
   //Escribe tu código aquí
+  //convertir numero en string
+  let numeroComoString=n.toString() //19===>'19'
+  if (numeroComoString[0]==='9') return true;//['n','9']
+  return false;
   
 }
 
@@ -157,6 +170,11 @@ function todosIguales(arreglo) {
   //Escriba la función todosIguales, que indique si todos los elementos de un arreglo son iguales:
   //retornar true, caso contrario retornar false.
   //Escribe tu código aquí  
+  for (let i=0 ; i< arreglo.length -1; i++)
+  if (arreglo[i]) !== arreglo[i+1]){
+    return false;
+  } 
+  return true;
   
 } 
 
@@ -166,6 +184,14 @@ function mesesDelAño(array) {
   // "Enero", "Marzo" y "Noviembre", guardarlo en nuevo array y retornarlo.
   //Si alguno de los meses no está, devolver: "No se encontraron los meses pedidos"
   // Tu código:
+  var nuevoArray=[];
+  for (let i=0; i<array.length; i++){
+    if (array[i]==="Enero"||"Marzo"||"Noviembre")
+    nuevoArray.push (array[i])
+    if nuevoArray.length<3 return "No se encontraron los meses pedidos";
+    
+  }
+  return nuevoArray;
 }
 
 
@@ -173,17 +199,36 @@ function mayorACien(array) {
   //La función recibe un array con enteros entre 0 y 200. Recorrer el array y guardar en un nuevo array sólo los
   //valores mayores a 100 (no incluye el 100). Finalmente devolver el nuevo array.
   // Tu código:
+  var nuevoArray[];
+  for (let i=o ; i< array.length; i++){
+    if (array[i]>100)
+    nuevoArray.push[array[i]]
+  }
+  return nuevoArray;
 }
 
 
+
 function breakStatement(numero) {
-  //Iterar en un bucle aumentando en 2 el numero recibido hasta un límite de 10 veces.
+  //Iterar en un bucle aumentando en  2 el numero recibido hasta un límite de 10 veces.
+  //4+2=6 =>6+2=8...10 veces
   //Guardar cada nuevo valor en un array. 
   //Devolver el array
   //Si en algún momento el valor de la suma y la cantidad de iteraciones coinciden, debe interrumpirse la ejecución y 
   //devolver: "Se interrumpió la ejecución"
   //Pista: usá el statement 'break'
   // Tu código:
+  var array[];
+  var valorSuma= numero;
+  for (let i=1; i<=10 ; i++){
+    valorSuma= valorSuma +2 // otra opcion valor +=2
+    if (valorSuma ===i){
+      break;
+    }
+    array.push (valorSuma);
+  }
+  if (i===10) return array;
+  return "Se interrumpió la ejecución";
 }
 
 
@@ -194,6 +239,14 @@ function continueStatement(numero) {
   //Cuando el número de iteraciones alcance el valor 5, no se suma en ese caso y se continua con la siguiente iteración
   //Pista: usá el statement 'continue'
   // Tu código:
+  var nuevoValor= numero;
+  var arreglo[];
+  for ( var i=1 ; i<= 10; i++){
+    if (i===5) continue;
+    nuevoValor +=2 // nuevoValor= nuevoValor+2
+    array.push (nuevoValor);
+  }
+  return array;
 }
 
 
@@ -221,4 +274,4 @@ module.exports = {
   mayorACien,
   breakStatement,
   continueStatement
-};
+}
